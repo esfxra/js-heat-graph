a349b5f66d51f781f6739ddae86cd0b727e828ca
Diego Serrano <diegoserranor@gmail.com>
2021-08-03 14:21:31 -0500

    Add draft for graph config

---
+++ config.js
@@ -0,0 +1,10 @@
+// Reference: https://github.com/laucyun/calendar-contributions-map
+
+const config = {
+  source: "", // json, google sheets
+  data: [], // json data, google sheets URL
+  colorRange: [],
+  selector: '#contributions-map',
+}
+
+export { config };
